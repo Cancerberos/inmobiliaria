d6ad687deb93ee3a5587f95790520e4c92d54ce3
Manuel Castillo <mjcv7@hotmail.com>
2022-06-05 22:18:02 -0300

    Crear clase paramétrica Característica #11

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/caracteristica/Caracteristica.java
@@ -0,0 +1,67 @@
+package domainapp.modules.simple.dom.caracteristica;
+
+import lombok.*;
+import org.apache.isis.applib.annotation.*;
+import org.apache.isis.applib.jaxb.PersistentEntityAdapter;
+import org.apache.isis.applib.services.message.MessageService;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.applib.services.title.TitleService;
+import org.jetbrains.annotations.NotNull;
+
+import javax.inject.Inject;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.VersionStrategy;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import static org.apache.isis.applib.annotation.SemanticsOf.NON_IDEMPOTENT_ARE_YOU_SURE;
+
+@javax.jdo.annotations.PersistenceCapable(
+        schema = "Inmobiliaria",
+        identityType= IdentityType.DATASTORE)
+@javax.jdo.annotations.Unique(
+        name = "caracteristica_descripcion_UNQ", members = {"descripcion"}
+)
+@javax.jdo.annotations.DatastoreIdentity(strategy= IdGeneratorStrategy.IDENTITY, column="id")
+@javax.jdo.annotations.Version(strategy= VersionStrategy.DATE_TIME, column="version")
+
+@DomainObject(logicalTypeName = "simple.caracteristica", entityChangePublishing = Publishing.ENABLED)
+@DomainObjectLayout()
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@XmlJavaTypeAdapter(PersistentEntityAdapter.class)
+@ToString(onlyExplicitlyIncluded = true)
+public class Caracteristica implements Comparable<Caracteristica>{
+
+    @Title
+    @Getter
+    @Setter
+    @ToString.Include
+    @PropertyLayout(fieldSetId = "descripcion", sequence = "1")
+    private String descripcion;
+
+    public Caracteristica(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Action(semantics = NON_IDEMPOTENT_ARE_YOU_SURE)
+    @ActionLayout(
+            position = ActionLayout.Position.PANEL,
+            describedAs = "Deletes this object from the persistent datastore")
+    public void delete() {
+        final String title = titleService.titleOf(this);
+        messageService.informUser(String.format("'%s' deleted", title));
+        repositoryService.removeAndFlush(this);
+    }
+
+    @Override
+    public int compareTo(@NotNull Caracteristica o) {
+        return 0;
+    }
+
+    @Inject
+    RepositoryService repositoryService;
+    @Inject
+    TitleService titleService;
+    @Inject
+    MessageService messageService;
+}
---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/caracteristica/CaracteristicaRepositorio.java
@@ -0,0 +1,40 @@
+package domainapp.modules.simple.dom.caracteristica;
+
+import org.apache.isis.applib.annotation.*;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.persistence.jdo.applib.services.JdoSupportService;
+
+import javax.inject.Inject;
+import javax.jdo.JDOQLTypedQuery;
+
+@DomainService(
+        nature = NatureOfService.VIEW,
+        logicalTypeName = "simple.Repositorio"
+)
+@javax.annotation.Priority(PriorityPrecedence.EARLY)
+@lombok.RequiredArgsConstructor(onConstructor_ = {@Inject} )
+public class CaracteristicaRepositorio {
+
+
+    final RepositoryService repositoryService;
+    final JdoSupportService jdoSupportService;
+
+
+    @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
+    @ActionLayout(promptStyle = PromptStyle.DIALOG_SIDEBAR)
+    public Caracteristica createCaracteristica(
+            final String descripcion) {
+        return repositoryService.persist(new Caracteristica(descripcion));
+    }
+
+    @Programmatic
+    public void ping() {
+        JDOQLTypedQuery<Caracteristica> q = jdoSupportService.newTypesafeQuery(Caracteristica.class);
+        final QCaracteristica candidate = QCaracteristica.candidate();
+        q.range(0,2);
+        q.orderBy(candidate.descripcion.asc());
+        q.executeList();
+    }
+
+
+}
