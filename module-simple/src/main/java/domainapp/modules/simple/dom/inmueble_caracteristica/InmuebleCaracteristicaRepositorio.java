ddefc3b563667c49e434ecfcc0fa068c14a4d748
Manuel Castillo <mjcv7@hotmail.com>
2022-06-05 23:05:36 -0300

    Crear clase paramétrica Inmuebles_Característica #11

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/inmueble_caracteristica/InmuebleCaracteristicaRepositorio.java
@@ -0,0 +1,43 @@
+package domainapp.modules.simple.dom.inmueble_caracteristica;
+
+import domainapp.modules.simple.dom.caracteristica.Caracteristica;
+import domainapp.modules.simple.dom.inmobiliaria.Inmobiliaria;
+import domainapp.modules.simple.dom.inmobiliaria.QInmobiliaria;
+import org.apache.isis.applib.annotation.*;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.persistence.jdo.applib.services.JdoSupportService;
+
+import javax.inject.Inject;
+import javax.jdo.JDOQLTypedQuery;
+
+@DomainService(
+        nature = NatureOfService.VIEW,
+        logicalTypeName = "simple.InmuebleCaracteristicaRepositorio"
+)
+@javax.annotation.Priority(PriorityPrecedence.EARLY)
+@lombok.RequiredArgsConstructor(onConstructor_ = {@Inject} )
+public class InmuebleCaracteristicaRepositorio {
+
+    final RepositoryService repositoryService;
+    final JdoSupportService jdoSupportService;
+
+
+    @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
+    @ActionLayout(promptStyle = PromptStyle.DIALOG_SIDEBAR)
+    public InmuebleCaracteristica createInmuebleCaracteristica(
+            final Caracteristica caracteristica,
+            final int cantidad) {
+        return repositoryService.persist(new InmuebleCaracteristica(caracteristica, cantidad));
+    }
+
+    @Programmatic
+    public void ping() {
+        JDOQLTypedQuery<InmuebleCaracteristica> q = jdoSupportService.newTypesafeQuery(InmuebleCaracteristica.class);
+        final QInmuebleCaracteristica candidate = QInmuebleCaracteristica.candidate();
+        q.range(0,2);
+        q.orderBy(candidate.cantidad.asc());
+        q.executeList();
+    }
+
+
+}
---
+++ module-simple/target/generated-sources/annotations/domainapp/modules/simple/dom/inmueble_caracteristica/QInmuebleCaracteristica.java
@@ -0,0 +1,64 @@
+package domainapp.modules.simple.dom.inmueble_caracteristica;
+
+import javax.annotation.processing.Generated;
+import javax.jdo.query.*;
+import org.datanucleus.api.jdo.query.*;
+
+@Generated(value="org.datanucleus.jdo.query.JDOQueryProcessor")
+public class QInmuebleCaracteristica extends PersistableExpressionImpl<InmuebleCaracteristica> implements PersistableExpression<InmuebleCaracteristica>
+{
+    public static final QInmuebleCaracteristica jdoCandidate = candidate("this");
+
+    public static QInmuebleCaracteristica candidate(String name)
+    {
+        return new QInmuebleCaracteristica(null, name, 5);
+    }
+
+    public static QInmuebleCaracteristica candidate()
+    {
+        return jdoCandidate;
+    }
+
+    public static QInmuebleCaracteristica parameter(String name)
+    {
+        return new QInmuebleCaracteristica(InmuebleCaracteristica.class, name, ExpressionType.PARAMETER);
+    }
+
+    public static QInmuebleCaracteristica variable(String name)
+    {
+        return new QInmuebleCaracteristica(InmuebleCaracteristica.class, name, ExpressionType.VARIABLE);
+    }
+
+    public final domainapp.modules.simple.dom.caracteristica.QCaracteristica caracteristica;
+    public final NumericExpression<Integer> cantidad;
+    public final ObjectExpression<org.apache.isis.applib.services.repository.RepositoryService> repositoryService;
+    public final ObjectExpression<org.apache.isis.applib.services.title.TitleService> titleService;
+    public final ObjectExpression<org.apache.isis.applib.services.message.MessageService> messageService;
+
+    public QInmuebleCaracteristica(PersistableExpression parent, String name, int depth)
+    {
+        super(parent, name);
+        if (depth > 0)
+        {
+            this.caracteristica = new domainapp.modules.simple.dom.caracteristica.QCaracteristica(this, "caracteristica", depth-1);
+        }
+        else
+        {
+            this.caracteristica = null;
+        }
+        this.cantidad = new NumericExpressionImpl<Integer>(this, "cantidad");
+        this.repositoryService = new ObjectExpressionImpl<org.apache.isis.applib.services.repository.RepositoryService>(this, "repositoryService");
+        this.titleService = new ObjectExpressionImpl<org.apache.isis.applib.services.title.TitleService>(this, "titleService");
+        this.messageService = new ObjectExpressionImpl<org.apache.isis.applib.services.message.MessageService>(this, "messageService");
+    }
+
+    public QInmuebleCaracteristica(Class type, String name, ExpressionType exprType)
+    {
+        super(type, name, exprType);
+        this.caracteristica = new domainapp.modules.simple.dom.caracteristica.QCaracteristica(this, "caracteristica", 5);
+        this.cantidad = new NumericExpressionImpl<Integer>(this, "cantidad");
+        this.repositoryService = new ObjectExpressionImpl<org.apache.isis.applib.services.repository.RepositoryService>(this, "repositoryService");
+        this.titleService = new ObjectExpressionImpl<org.apache.isis.applib.services.title.TitleService>(this, "titleService");
+        this.messageService = new ObjectExpressionImpl<org.apache.isis.applib.services.message.MessageService>(this, "messageService");
+    }
+}
