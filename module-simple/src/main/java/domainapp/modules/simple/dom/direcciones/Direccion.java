2d049f48d3d13264a02dd55d6ea528daf6171c4f
Dario Torres <torres.dario@oca.com.ar>
2022-06-06 22:04:05 -0300

    se crean las entidades Direccion/Provincia #11

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/direcciones/Direccion.java
@@ -0,0 +1,129 @@
+package domainapp.modules.simple.dom.direcciones;
+
+
+import domainapp.modules.simple.types.Name;
+import lombok.*;
+import org.apache.isis.applib.annotation.*;
+import org.apache.isis.applib.jaxb.PersistentEntityAdapter;
+import org.apache.isis.applib.services.message.MessageService;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.applib.services.title.TitleService;
+import org.jetbrains.annotations.NotNull;
+
+import javax.inject.Inject;
+import javax.jdo.annotations.*;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import static org.apache.isis.applib.annotation.SemanticsOf.NON_IDEMPOTENT_ARE_YOU_SURE;
+@PersistenceCapable(
+        schema = "Inmobiliaria",
+        identityType=IdentityType.DATASTORE)
+@Queries({
+        @Query(
+                name = "findAll", language = "JDOQL",
+                value = "SELECT "
+                        + " FROM domainapp.modules.simple.dom.direccion.Direccion "
+                        + "ORDER BY calle ASC")
+})
+@Unique(
+        name = "calle_calle_UNQ", members = {"calle"}
+)
+@DatastoreIdentity(strategy=IdGeneratorStrategy.IDENTITY, column="id")
+@Version(strategy= VersionStrategy.DATE_TIME, column="version")
+@DomainObject(editing = Editing.DISABLED)
+//@DomainObject(logicalTypeName = "simple.Direccion", entityChangePublishing = Publishing.ENABLED)
+@DomainObjectLayout()
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@XmlJavaTypeAdapter(PersistentEntityAdapter.class)
+@ToString(onlyExplicitlyIncluded = true)
+public class Direccion implements Comparable<Direccion>{
+
+
+    @Title
+    @Name
+    @Getter @Setter @ToString.Include
+    @PropertyLayout(fieldSetId = "provincia", sequence = "1")
+    private String provincia;
+
+    @Getter @Setter
+    @Property(commandPublishing = Publishing.ENABLED, executionPublishing = Publishing.ENABLED)
+    @PropertyLayout(fieldSetId = "localidad", sequence = "2")
+    private String localidad;
+
+    @Getter @Setter
+    @Property(commandPublishing = Publishing.ENABLED, executionPublishing = Publishing.ENABLED)
+    @PropertyLayout(fieldSetId = "cp", sequence = "3")
+    private int cp;
+
+    @Getter @Setter
+    @Property(commandPublishing = Publishing.ENABLED, executionPublishing = Publishing.ENABLED)
+    @PropertyLayout(fieldSetId = "calle", sequence = "4")
+    private String calle;
+
+    @Getter @Setter
+    @Property(commandPublishing = Publishing.ENABLED, executionPublishing = Publishing.ENABLED)
+    @PropertyLayout(fieldSetId = "numero", sequence = "5")
+    private int numero;
+
+    @Getter @Setter
+    @Property(commandPublishing = Publishing.ENABLED, executionPublishing = Publishing.ENABLED)
+    @PropertyLayout(fieldSetId = "edificacion", sequence = "6")
+    private String edificacion;
+
+    @Getter @Setter
+    @Property(commandPublishing = Publishing.ENABLED, executionPublishing = Publishing.ENABLED)
+    @PropertyLayout(fieldSetId = "piso", sequence = "7")
+    private String  piso;
+
+    @Getter @Setter
+    @Property(commandPublishing = Publishing.ENABLED, executionPublishing = Publishing.ENABLED)
+    @PropertyLayout(fieldSetId = "departamento", sequence = "8")
+    private String departamento;
+
+    @Getter @Setter
+    @Property(commandPublishing = Publishing.ENABLED, executionPublishing = Publishing.ENABLED)
+    @PropertyLayout(fieldSetId = "latitud", sequence = "9")
+    private String latitud;
+
+    @Getter @Setter
+    @Property(commandPublishing = Publishing.ENABLED, executionPublishing = Publishing.ENABLED)
+    @PropertyLayout(fieldSetId = "longitud", sequence = "10")
+    private String longitud;
+
+
+    public Direccion(String provincia, String localidad, int cp, int numero, String edificacion, String piso, String departamento, String latitud, String longitud) {
+        this.provincia = provincia;
+        this.localidad = localidad;
+        this.cp = cp;
+        this.calle = calle;
+        this.numero = numero;
+        this.edificacion = edificacion;
+        this.piso = piso;
+        this.departamento = departamento;
+        this.latitud = latitud;
+        this.longitud = longitud;
+
+    }
+
+    @Action(semantics = NON_IDEMPOTENT_ARE_YOU_SURE)
+    @ActionLayout(
+            position = ActionLayout.Position.PANEL,
+            describedAs = "Elimina este objeto del almacén de datos persistente")
+    public void delete() {
+        final String title = titleService.titleOf(this);
+        messageService.informUser(String.format("'%s' deleted", title));
+        repositoryService.removeAndFlush(this);
+    }
+
+    @Override
+    public int compareTo(@NotNull Direccion o) {
+        return 0;
+    }
+
+    @Inject
+    RepositoryService repositoryService;
+    @Inject
+    TitleService titleService;
+    @Inject
+    MessageService messageService;
+
+}
---
+++ module-simple/target/generated-sources/annotations/domainapp/modules/simple/dom/provincia/QProvincia.java
@@ -0,0 +1,54 @@
+package domainapp.modules.simple.dom.provincia;
+
+import javax.annotation.processing.Generated;
+import javax.jdo.query.*;
+import org.datanucleus.api.jdo.query.*;
+
+@Generated(value="org.datanucleus.jdo.query.JDOQueryProcessor")
+public class QProvincia extends PersistableExpressionImpl<Provincia> implements PersistableExpression<Provincia>
+{
+    public static final QProvincia jdoCandidate = candidate("this");
+
+    public static QProvincia candidate(String name)
+    {
+        return new QProvincia(null, name, 5);
+    }
+
+    public static QProvincia candidate()
+    {
+        return jdoCandidate;
+    }
+
+    public static QProvincia parameter(String name)
+    {
+        return new QProvincia(Provincia.class, name, ExpressionType.PARAMETER);
+    }
+
+    public static QProvincia variable(String name)
+    {
+        return new QProvincia(Provincia.class, name, ExpressionType.VARIABLE);
+    }
+
+    public final StringExpression descripcion;
+    public final ObjectExpression<org.apache.isis.applib.services.repository.RepositoryService> repositoryService;
+    public final ObjectExpression<org.apache.isis.applib.services.title.TitleService> titleService;
+    public final ObjectExpression<org.apache.isis.applib.services.message.MessageService> messageService;
+
+    public QProvincia(PersistableExpression parent, String name, int depth)
+    {
+        super(parent, name);
+        this.descripcion = new StringExpressionImpl(this, "descripcion");
+        this.repositoryService = new ObjectExpressionImpl<org.apache.isis.applib.services.repository.RepositoryService>(this, "repositoryService");
+        this.titleService = new ObjectExpressionImpl<org.apache.isis.applib.services.title.TitleService>(this, "titleService");
+        this.messageService = new ObjectExpressionImpl<org.apache.isis.applib.services.message.MessageService>(this, "messageService");
+    }
+
+    public QProvincia(Class type, String name, ExpressionType exprType)
+    {
+        super(type, name, exprType);
+        this.descripcion = new StringExpressionImpl(this, "descripcion");
+        this.repositoryService = new ObjectExpressionImpl<org.apache.isis.applib.services.repository.RepositoryService>(this, "repositoryService");
+        this.titleService = new ObjectExpressionImpl<org.apache.isis.applib.services.title.TitleService>(this, "titleService");
+        this.messageService = new ObjectExpressionImpl<org.apache.isis.applib.services.message.MessageService>(this, "messageService");
+    }
+}
